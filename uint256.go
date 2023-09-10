6f50148646970e4c4aa054765bc7c7cfb66ee8c8
ICHINOSE Shogo <shogo82148@gmail.com>
2023-09-10 09:56:13 +0900

    implement divMod128

--- uint256.go
+++ uint256.go
@@ -42,12 +42,63 @@ func mul128(x, y int128.Uint128) uint256 {
 	return uint256{a, b, c, d}
 }
 
+func (x uint256) mul128(y int128.Uint128) uint256 {
+	h1, l1 := bits.Mul64(x.d, y.L)
+	h2, l2 := bits.Mul64(x.c, y.L)
+	h3, l3 := bits.Mul64(x.b, y.L)
+	_, l4 := bits.Mul64(x.a, y.L)
+
+	h5, l5 := bits.Mul64(x.d, y.H)
+	h6, l6 := bits.Mul64(x.c, y.H)
+	_, l7 := bits.Mul64(x.b, y.H)
+
+	//     x.a  x.b  x.c  x.d
+	//               y.H  y.L
+	//     ------------------
+	//                h1   l1
+	//           h2   l2
+	//      h3   l3
+	//      l4
+	//           h5   l5
+	//      h6   l6
+	//      l7
+	//-----------------------
+	//       a    b    c    d
+
+	a := h3
+	b := h2
+	c := h1
+	d := l1
+
+	var carry uint64
+	c, carry = bits.Add64(c, l2, 0)
+	b, carry = bits.Add64(b, l3, carry)
+	a, _ = bits.Add64(a, l4, carry)
+
+	c, carry = bits.Add64(c, l5, 0)
+	b, carry = bits.Add64(b, l6, carry)
+	a, _ = bits.Add64(a, l7, carry)
+
+	b, carry = bits.Add64(b, h5, 0)
+	a, _ = bits.Add64(a, h6, carry)
+	return uint256{a, b, c, d}
+}
+
 func (x uint256) add(y uint256) uint256 {
 	var carry uint64
 	x.d, carry = bits.Add64(x.d, y.d, 0)
 	x.c, carry = bits.Add64(x.c, y.c, carry)
 	x.b, carry = bits.Add64(x.b, y.b, carry)
 	x.a, _ = bits.Add64(x.a, y.a, carry)
+	return x
+}
+
+func (x uint256) sub(y uint256) uint256 {
+	var borrow uint64
+	x.d, borrow = bits.Sub64(x.d, y.d, 0)
+	x.c, borrow = bits.Sub64(x.c, y.c, borrow)
+	x.b, borrow = bits.Sub64(x.b, y.b, borrow)
+	x.a, _ = bits.Sub64(x.a, y.a, borrow)
 	return x
 }
 
@@ -118,6 +169,49 @@ func (x uint256) leadingZeros() int {
 
 func (x uint256) isZero() bool {
 	return (x.a | x.b | x.c | x.d) == 0
+}
+
+func (x uint256) divMod128(y int128.Uint128) (div uint256, mod int128.Uint128) {
+	if (y.H | y.L) == 0 {
+		panic("division by zero")
+	}
+	if y.H == 0 {
+		// fast path for uint256 / uint64
+		div.a = x.a / y.L
+		q, r := bits.Div64(x.a%y.L, x.b, y.L)
+		div.b = q
+		q, r = bits.Div64(r, x.c, y.L)
+		div.c = q
+		q, r = bits.Div64(r, x.d, y.L)
+		div.d = q
+		mod.L = r
+		return
+	}
+
+	n := bits.LeadingZeros64(y.H)
+	x2 := x.rsh(1)
+	y2 := y.Lsh(uint(n))
+
+	div.a = x2.a / y2.H
+	q, r := bits.Div64(x2.a%y2.H, x2.b, y2.H)
+	div.b = q
+	q, r = bits.Div64(r, x2.c, y2.H)
+	div.c = q
+	q, _ = bits.Div64(r, x2.d, y2.H)
+	div.d = q
+	div = div.rsh(uint(63 - n))
+	if !div.isZero() {
+		div = div.sub(uint256{0, 0, 0, 1})
+	}
+
+	z := div.mul128(y)
+	mod256 := x.sub(z)
+	mod = int128.Uint128{H: mod256.c, L: mod256.d}
+	if mod.Cmp(y) >= 0 {
+		div = div.add(uint256{0, 0, 0, 1})
+		mod = mod.Sub(y)
+	}
+	return
 }
 
 func (x uint256) GoString() string {
--- uint256_test.go
+++ uint256_test.go
@@ -1,6 +1,10 @@
 package float128
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/shogo82148/int128"
+)
 
 func TestUint256Rsh(t *testing.T) {
 	tests := []struct {
@@ -60,6 +64,35 @@ func TestUint256Lsh(t *testing.T) {
 		got := tt.input.lsh(tt.n)
 		if got != tt.want {
 			t.Errorf("%#v.rsh(%d)\n got: %#v\nwant: %#v", tt.input, tt.n, got, tt.want)
+		}
+	}
+}
+
+func TestUint256DivMod(t *testing.T) {
+	tests := []struct {
+		a uint256
+		b int128.Uint128
+		q uint256
+		r int128.Uint128
+	}{
+		{
+			uint256{0x01234567_89abcdef, 0x01234567_89abcdef, 0x01234567_89abcdef, 0x01234567_89abcdef},
+			int128.Uint128{L: 0x1_0000_0000},
+			uint256{0x000000000_1234567, 0x89abcdef_01234567, 0x89abcdef_01234567, 0x89abcdef_01234567},
+			int128.Uint128{L: 0x89abcdef},
+		},
+		{
+			uint256{0x01234567_89abcdef, 0x01234567_89abcdef, 0x01234567_89abcdef, 0x01234567_89abcdef},
+			int128.Uint128{H: 1},
+			uint256{0, 0x01234567_89abcdef, 0x01234567_89abcdef, 0x01234567_89abcdef},
+			int128.Uint128{L: 0x01234567_89abcdef},
+		},
+	}
+
+	for _, tt := range tests {
+		q, r := tt.a.divMod128(tt.b)
+		if q != tt.q || r != tt.r {
+			t.Errorf("%#v.divMod(%#v)\n got: %#v, %#v\nwant: %#v, %#v", tt.a, tt.b, q, r, tt.q, tt.r)
 		}
 	}
 }
