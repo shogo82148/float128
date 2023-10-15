f46648874c8722b25f82197dda8becf6dfdd10de
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-16 05:15:17 +0900

    support hexadecimal format

--- strconv.go
+++ strconv.go
@@ -1,6 +1,8 @@
 package float128
 
-import "github.com/shogo82148/int128"
+import (
+	"github.com/shogo82148/int128"
+)
 
 func (x Float128) Text(fmt byte, prec int) string {
 	return string(x.Append(nil, fmt, prec))
@@ -19,6 +21,8 @@ func (x Float128) Append(buf []byte, fmt byte, prec int) []byte {
 	switch fmt {
 	case 'b':
 		return x.appendBin(buf)
+	case 'x', 'X':
+		return x.appendHex(buf, fmt, prec)
 	}
 
 	return buf
@@ -64,6 +68,95 @@ func (x Float128) appendBin(buf []byte) []byte {
 		fallthrough
 	default:
 		buf = append(buf, '0'+byte(exp))
+	}
+	return buf
+}
+
+func (x Float128) appendHex(buf []byte, fmt byte, prec int) []byte {
+	sign, exp, frac := x.split()
+	if sign != 0 {
+		buf = append(buf, '-')
+	}
+	buf = append(buf, '0', fmt)
+	if frac == (int128.Uint128{}) {
+		buf = append(buf, '0')
+		if prec > 0 {
+			buf = append(buf, '.')
+			for i := 0; i < prec; i++ {
+				buf = append(buf, '0')
+			}
+		}
+		buf = append(buf, fmt-('x'-'p'), '+', '0', '0')
+		return buf
+	}
+
+	if prec < 0 {
+		var tmp [30]byte
+		digits := frac.Append(tmp[:0], 16)
+
+		// find the last non-zero digit
+		n := len(digits) - 1
+		for ; n >= 0 && digits[n] == '0'; n-- {
+		}
+
+		// print the digits
+		buf = append(buf, '1')
+		if n > 0 {
+			buf = append(buf, '.')
+			buf = append(buf, digits[1:n+1]...)
+		}
+	} else if prec < 28 {
+		// round nearest even
+		shift := uint(shift128 - 4*prec)
+		one := int128.Uint128{L: 1}
+		offset := one.Lsh(shift - 1).Sub(one)
+		frac = frac.Add(offset).Add(frac.Rsh(shift).And(one))
+		if frac.H >= 2<<(shift128-64) {
+			exp++
+			frac = frac.Rsh(1)
+		}
+
+		// print the digits
+		buf = append(buf, '1')
+		if prec > 0 {
+			buf = append(buf, '.')
+			var tmp [30]byte
+			digits := frac.Append(tmp[:0], 16)
+			buf = append(buf, digits[1:prec+1]...)
+		}
+	} else {
+		var tmp [30]byte
+		digits := frac.Append(tmp[:0], 16)
+		buf = append(buf, '1', '.')
+		buf = append(buf, digits[1:29]...)
+		for i := 28; i < prec; i++ {
+			buf = append(buf, '0')
+		}
+	}
+
+	buf = append(buf, fmt-('x'-'p'))
+	if exp >= 0 {
+		buf = append(buf, '+')
+	} else {
+		buf = append(buf, '-')
+		exp = -exp
+	}
+
+	switch {
+	case exp >= 10000:
+		buf = append(buf, '0'+byte(exp/10000))
+		exp %= 10000
+		fallthrough
+	case exp >= 1000:
+		buf = append(buf, '0'+byte(exp/1000))
+		exp %= 1000
+		fallthrough
+	case exp >= 100:
+		buf = append(buf, '0'+byte(exp/100))
+		exp %= 100
+		fallthrough
+	default:
+		buf = append(buf, byte((exp/10)%10)+'0', byte(exp%10)+'0')
 	}
 	return buf
 }
--- strconv_test.go
+++ strconv_test.go
@@ -9,8 +9,21 @@ func TestText(t *testing.T) {
 		prec int
 		s    string
 	}{
+		/****** binary format ******/
 		{FromFloat64(0), 'b', 0, "0p-16494"},
 		{FromFloat64(1), 'b', 0, "5192296858534827628530496329220096p-112"},
+
+		/****** hexadecimal format ******/
+		{FromFloat64(0), 'x', -1, "0x0p+00"},
+		{FromFloat64(1), 'x', -1, "0x1p+00"},
+		{FromFloat64(0.1), 'x', -1, "0x1.999999999999ap-04"},
+		{FromFloat64(1).Quo(FromFloat64(10)), 'x', -1, "0x1.999999999999999999999999999ap-04"},
+
+		{FromFloat64(0.1), 'x', 0, "0x1p-03"},
+		{FromFloat64(0.1), 'x', 2, "0x1.9ap-04"},
+		{FromFloat64(1).Quo(FromFloat64(10)), 'x', 28, "0x1.999999999999999999999999999ap-04"},
+		{FromFloat64(1).Quo(FromFloat64(10)), 'x', 30, "0x1.999999999999999999999999999a00p-04"},
+		{Float128{0, 1}, 'x', 1, "0x1.0p-16494"},
 	}
 
 	for _, tt := range tests {
